a1f7eb85d6f5480e0836e62d2e7baf7a773d153b
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-08-19 00:40:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const elements = document.getElementsByTagName("input");
+
 function getFormvalue() {
+	let fName = elements[0].value;
+	let lName = elements[1].value;
     //Write your code here
+	alert(fName + " " + lName);
 
 }
